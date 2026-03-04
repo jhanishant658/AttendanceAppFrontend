8f690e505ad067d35998dac3e3fda41da8da46d9
jhanishant658 <jhanishant658@gmail.com>
2026-03-04 17:10:35 +0530

    Initial commit - React frontend for attendance system

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-5xl text-yellow-400 font-bold">
+        Tailwind is working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
